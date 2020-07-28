ad2b2fee9ab62479b8a273dff1d6eccffe3623a4
Dhanavel <54678211+Dhanuvel163@users.noreply.github.com>
2020-07-28 23:21:41 +0530

    changes

--- server/config.js
+++ server/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   database:
-    'mongodb+srv://dhanuram99:saidhanavel@dhanuprojects-dc6z3.mongodb.net/PEPLAW?retryWrites=true&w=majority',
+    'mongoconnect url',
   port: 3030,
   secret: 'hellodhanuvel123'
 };
