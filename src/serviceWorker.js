c76b4566ab717b40b45e512df1e88e10b9d5256a
Dhanuvel163 <dhanuram99@gmail.com>
2021-01-16 01:01:25 +0530

    SERVICE WORKER

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -122,84 +122,79 @@ export function unregister() {
 
 //-------------------------------------------------------//
 
-// self.addEventListener('fetch', (event) => {
-//   event.respondWith(
-//     new Response('Hello')
-//   )
-//   const url = new URL(event.request.url)
-//   if(url.pathname.endsWith('.webp')){
-//     event.respondWith(
-//       fetch('/assets/lawlogo192.webp')
-//     )
-//   }
-// });
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    new Response('Hello')
+  )
+  const url = new URL(event.request.url)
+  if(url.pathname.endsWith('.webp')){
+    event.respondWith(
+      fetch('/assets/lawlogo192.webp')
+    )
+  }
+});
 
-// const PRECACHE = 'precache';
-// const RUNTIME = 'runtime';
-// const PRECACHE_URLS = [
-//   './',
-//   '/offline.html',
-//   '/assets/add1.svg',
-//   '/assets/edit.svg',
-//   '/assets/home1.webp',
-//   '/assets/home2.webp',
-//   '/assets/home3.webp',
-//   '/assets/home4.webp',
-//   '/assets/homecardbg.webp',
-//   '/assets/lawlogo64.webp',
-//   '/assets/lawlogo192.webp',
-//   '/assets/lawlogo512.webp',
-//   '/assets/lawyersignup.svg',
-//   '/assets/loginlawyer.svg',
-//   '/assets/userlogin.svg',
-//   '/assets/usersignup.svg',
-// ];
+const PRECACHE = 'precache';
+const RUNTIME = 'runtime';
+const PRECACHE_URLS = [
+  './',
+  '/offline.html',
+  '/assets/add1.svg',
+  '/assets/edit.svg',
+  '/assets/home1.webp',
+  '/assets/home2.webp',
+  '/assets/home3.webp',
+  '/assets/home4.webp',
+  '/assets/homecardbg.webp',
+  '/assets/lawlogo64.webp',
+  '/assets/lawlogo192.webp',
+  '/assets/lawlogo512.webp',
+  '/assets/lawyersignup.svg',
+  '/assets/loginlawyer.svg',
+  '/assets/userlogin.svg',
+  '/assets/usersignup.svg',
+];
 
-// self.addEventListener('install', event => {
-//   event.waitUntil(
-//     caches.open(PRECACHE)
-//       .then(cache => cache.addAll(PRECACHE_URLS))
-//       .then(self.skipWaiting())
-//   );
-// });
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(PRECACHE)
+      .then(cache => cache.addAll(PRECACHE_URLS))
+      .then(self.skipWaiting())
+  );
+});
 
-// self.addEventListener('activate', event => {
-//   const currentCaches = [PRECACHE, RUNTIME];
-//   event.waitUntil(
-//     caches.keys().then(cacheNames => {
-//       return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
-//     }).then(cachesToDelete => {
-//       return Promise.all(cachesToDelete.map(cacheToDelete => {
-//         return caches.delete(cacheToDelete);
-//       }));
-//     }).then(() => self.clients.claim())
-//   );
-// });
+self.addEventListener('activate', event => {
+  const currentCaches = [PRECACHE, RUNTIME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
+    }).then(cachesToDelete => {
+      return Promise.all(cachesToDelete.map(cacheToDelete => {
+        return caches.delete(cacheToDelete);
+      }));
+    }).then(() => self.clients.claim())
+  );
+});
 
-// // The fetch handler serves responses for same-origin resources from a cache.
-// // If no response is found, it populates the runtime cache with the response
-// // from the network before returning it to the page.
-// self.addEventListener('fetch', event => {
-//   // Skip cross-origin requests, like those for Google Analytics.
-//   if (event.request.url.startsWith(self.location.origin)) {
-//     event.respondWith(
-//       caches.match(event.request).then(cachedResponse => {
-//         if (cachedResponse) {
-//           return cachedResponse;
-//         }
-//         return caches.open(RUNTIME).then(cache => {
-//           return fetch(event.request).then(response => {
-//             // Put a copy of the response in the runtime cache.
-//             return cache.put(event.request, response.clone()).then(() => {
-//               return response;
-//             });
-//           });
-//         });
-//       }).catch(()=>{
-//         if(event.request.mode == 'navigate'){
-//           return caches.match('/offline.html')
-//         }
-//       })
-//     );
-//   }
-// });
+self.addEventListener('fetch', event => {
+  if (event.request.url.startsWith(self.location.origin)) {
+    event.respondWith(
+      caches.match(event.request).then(cachedResponse => {
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+        return caches.open(RUNTIME).then(cache => {
+          return fetch(event.request).then(response => {
+            return cache.put(event.request, response.clone()).then(() => {
+              return response;
+            });
+          });
+        });
+      }).catch(()=>{
+        if(event.request.mode == 'navigate'){
+          return caches.match('/offline.html')
+        }
+      })
+    );
+  }
+});
